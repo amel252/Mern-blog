26fa040f91ec6e6f20e1f60eb000145061e19245
Amel252 <amelghanem25@gmail.com>
2025-07-03 15:18:49 +0200

    Création des pages et routes + component  Header

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,24 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Dashbord from "./pages/Dashbord";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+export default function App() {
     return (
-        <>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-        </>
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/dashbord" element={<Dashbord />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-up" element={<SignUp />} />
+            </Routes>
+        </BrowserRouter>
     );
 }
-
-export default App;
