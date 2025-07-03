ae3c41696c6dca2d40b801069f7c97f8b4a56426
Amel252 <amelghanem25@gmail.com>
2025-07-03 16:41:06 +0200

    Installation Flowbite , Création coponent header

--- front-end/src/pages/Projects.jsx
+++ front-end/src/pages/Projects.jsx
@@ -1,3 +1,9 @@
+import { Button } from "flowbite-react";
 export default function Projects() {
-    return <div>Projects</div>;
+    return (
+        <div>
+            <h1>Projects</h1>
+            <Button className="text-red-600 font-semibold">Clique me </Button>
+        </div>
+    );
 }
