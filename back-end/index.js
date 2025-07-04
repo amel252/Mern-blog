afc1aba5a5c40f860049a091e0c1a5d8a7ddc35a
Amel252 <amelghanem25@gmail.com>
2025-07-04 15:06:57 +0200

    installation de mongoose et dotenv et faire la connexion avec BDD

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,18 @@
 // si on veux créer une route , server  => on doit avoir express
 // si on veux créer un model => on doit avoir mongoose
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log("Connection reussie avec la BD");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 const app = express();
 
