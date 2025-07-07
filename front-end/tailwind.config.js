c1a53eb38c87d39675f04273ddc6642a570ff906
Amel252 <amelghanem25@gmail.com>
2025-07-07 15:22:26 +0200

    Ajout de fonctionnalité permettant l'inscription de l'utilisateur (handleSubmit, handleChange)

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,3 +1,17 @@
+// const flowbite = require("flowbite/plugin");
+
+// module.exports = {
+//     content: [
+//         "./index.html",
+//         "./src/**/*.{js,ts,jsx,tsx}",
+//         "./node_modules/flowbite/**/*.js",
+//         "./node_modules/flowbite-react/**/*.js",
+//     ],
+//     theme: {
+//         extend: {},
+//     },
+//     plugins: [flowbite],
+// };
 const flowbite = require("flowbite/plugin");
 
 module.exports = {
