c53db3b512412b5e5afa5e3fcca14ff33de8fa3c
Amel252 <amelghanem25@gmail.com>
2025-07-07 13:57:26 +0200

    Création de page SignIn (interface de connexion)

--- front-end/src/pages/SignUp.jsx
+++ front-end/src/pages/SignUp.jsx
@@ -1,5 +1,72 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Label, TextInput, Button } from "flowbite-react";
 
 export default function SignUp() {
-    return <div>SignUp</div>;
+    return (
+        <div className="min-h-screen mt-20">
+            <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+                {/* left */}
+                <div className="flex-1">
+                    <Link to="/" className="font-bold dark:text-white text-4xl">
+                        <span className="px-5 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                            Blog
+                        </span>
+                        web
+                    </Link>
+                    <p className="text-sm mt-5">
+                        Ceci est un projet de démonstration. Vous pouvez vous
+                        inscrire avec votre adresse e-mail et votre mot de passe
+                        ou via Google.
+                    </p>
+                </div>
+
+                {/* right */}
+                <div className="flex-1">
+                    <form className="flex flex-col gap-4">
+                        <div>
+                            <Label htmlFor="username" value="Votre nom" />
+                            <TextInput
+                                type="text"
+                                placeholder="Votre nom"
+                                id="username"
+                            />
+                        </div>
+                        <div>
+                            <Label htmlFor="email" value="Votre email" />
+                            <TextInput
+                                type="email"
+                                placeholder="name@company.com"
+                                id="email"
+                            />
+                        </div>
+                        <div>
+                            <Label
+                                htmlFor="password"
+                                value="Votre mot de passe"
+                            />
+                            <TextInput
+                                type="password"
+                                placeholder="Mot de passe"
+                                id="password"
+                            />
+                        </div>
+                        {/*le bouton dans une div stylée */}
+                        <div className="bg-white p-4 rounded-md ">
+                            <Button color="blue" type="submit">
+                                Connectez-vous
+                            </Button>
+                        </div>
+                    </form>
+
+                    <div className="flex gap-2 mt-5 text-sm">
+                        <span>Avez-vous un compte ?</span>
+                        <Link to="/sign-in" className="text-blue-500">
+                            Connectez-vous
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,13 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+const flowbite = require("flowbite/plugin");
+
+module.exports = {
     content: [
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
-        "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
-        "./node_modules/flowbite/**/*.{js,jsx,ts,tsx}", //
+        "./node_modules/flowbite/**/*.js",
+        "./node_modules/flowbite-react/**/*.js",
     ],
     theme: {
         extend: {},
     },
-    plugins: [require("flowbite/plugin")],
+    plugins: [flowbite],
 };
