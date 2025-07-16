746f20935589e1823178f2bfd0c7bd8ec7a0cfcc
Amel252 <amelghanem25@gmail.com>
2025-07-16 16:43:33 +0200

    Ajout contenu Page Profil (DashProfil)

--- front-end/src/components/DashSidebar.jsx
+++ front-end/src/components/DashSidebar.jsx
@@ -3,42 +3,6 @@ import { HiArrowSmRight, HiUser } from "react-icons/hi";
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
-// export default function DashSidebar() {
-//     const location = useLocation();
-//     const [tab, setTab] = useState();
-
-//     // chercher dans la tab de notre url
-//     useEffect(() => {
-//         const urlParams = new URLSearchParams(location.search);
-//         const tabFromUrl = urlParams.get("tab");
-//         if (tabFromUrl) {
-//             setTab(tabFromUrl);
-//         }
-//     }, [location.search]);
-//     return (
-//         <Sidebar className="w-full md:w-56">
-//             <Sidebar.Items>
-//                 <Sidebar.ItemGroup>
-//                     <Link to="/dashboard?tab=profile">
-//                         <Sidebar.Item
-//                             active={tab === "profile"}
-//                             icon={HiUser}
-//                             label="Utilisateur"
-//                             labelColor="dark"
-//                         >
-//                             Profil
-//                         </Sidebar.Item>
-//                     </Link>
-//                     <Link to="/dashboard?tab=profile">
-//                         <Sidebar.Item icon={HiArrowSmRight} label="Déconnexion">
-//                             Déconnexion
-//                         </Sidebar.Item>
-//                     </Link>
-//                 </Sidebar.ItemGroup>
-//             </Sidebar.Items>
-//         </Sidebar>
-//     );
-// }
 export default function DashSidebar() {
     const location = useLocation();
     const [tab, setTab] = useState();
