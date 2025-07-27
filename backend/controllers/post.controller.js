91f2e919158f9e5f1252917ba0d68a5e41afaf34
Amel252 <amelghanem25@gmail.com>
2025-07-27 22:44:09 +0200

    API permettant la récupération des Posts publiés

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -29,3 +29,47 @@ export const create = async (req, res, next) => {
         next(error);
     }
 };
+
+export const getPosts = async (req, res, next) => {
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 9;
+        const sortDirection = req.query.order === "asc" ? 1 : -1;
+        const posts = await Post.find({
+            ...(req.query.userId && { userId: req.query.userId }),
+            ...(req.query.category && { category: req.query.category }),
+            ...(req.query.slug && { slug: req.query.slug }),
+            ...(req.query.postId && { _id: req.query.postId }),
+            ...(req.query.searchTerm && {
+                $or: [
+                    // "insensible à la casse" si tu cherche en maj ou miniscule
+                    {
+                        title: {
+                            $regex: req.query.searchTerm,
+                            $options: "i",
+                        },
+                    },
+                ],
+            }),
+        })
+            .sort({ updateAt: sortDirection })
+            .skip(startIndex)
+            .limit(limit);
+        const totalPosts = await Post.countDocuments();
+        const now = new Date();
+
+        const oneMounthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+        );
+        const lastMounthPosts = await Post.countDocuments({
+            createAt: { $gte: oneMounthAgo },
+        });
+        res.status(200).json({
+            posts,
+            totalPosts,
+            lastMounthPosts,
+        });
+    } catch (error) {}
+};
