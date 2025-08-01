664ae0afd93ea0eac06ee6823b706438c37acaaf
Amel252 <amelghanem25@gmail.com>
2025-08-01 15:07:24 +0200

    Création de API permettant la récupération de tous les utilisateurs (getUsers)

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -3,6 +3,7 @@ import {
     test,
     updateUser,
     deleteUser,
+    getUsers,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
+router.get("/getusers", verifyToken, getUsers);
 
 export default router;
